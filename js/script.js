60629f10f8dc655485f19bc644f79437071bc43d
Parham Abolghasemi <parhamab17@gmail.com>
2022-08-23 17:09:06 +0430

    active keys styling

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ const $ = document;
 const keys = $.querySelectorAll(".key");
 keys.forEach((item) => {
   item.addEventListener("click", function () {
+    const selectedBtn = item.dataset.note;
     const noteAudio = document.getElementById(item.dataset.note);
     // remove delay on play
     noteAudio.currentTime = 0;
     noteAudio.play();
+    console.log(selectedBtn);
   });
 });
