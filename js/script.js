9ae08565193506553c7cae976bd621d18afa8e45
Parham Abolghasemi <parhamab17@gmail.com>
2022-08-23 17:13:10 +0430

    clean up codes

--- js/script.js
+++ js/script.js
@@ -3,11 +3,9 @@ const $ = document;
 const keys = $.querySelectorAll(".key");
 keys.forEach((item) => {
   item.addEventListener("click", function () {
-    const selectedBtn = item.dataset.note;
     const noteAudio = document.getElementById(item.dataset.note);
     // remove delay on play
     noteAudio.currentTime = 0;
     noteAudio.play();
-    console.log(selectedBtn);
   });
 });
