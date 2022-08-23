eb1229af711cd07cadd025170ce9c47f5d272537
Parham Abolghasemi <parhamab17@gmail.com>
2022-08-23 17:04:45 +0430

    bugs fixed

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ const keys = $.querySelectorAll(".key");
 keys.forEach((item) => {
   item.addEventListener("click", function () {
     const noteAudio = document.getElementById(item.dataset.note);
+    // remove delay on play
+    noteAudio.currentTime = 0;
     noteAudio.play();
   });
 });
