6298b33a2db7b230bd4ffca5ff38691ee5ea914f
Parham Abolghasemi <parhamab17@gmail.com>
2022-08-23 17:03:58 +0430

    play sound by clicking

--- js/script.js
+++ js/script.js
@@ -1,2 +1,9 @@
 // elements
 const $ = document;
+const keys = $.querySelectorAll(".key");
+keys.forEach((item) => {
+  item.addEventListener("click", function () {
+    const noteAudio = document.getElementById(item.dataset.note);
+    noteAudio.play();
+  });
+});
